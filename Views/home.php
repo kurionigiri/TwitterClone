0841ca7afe45e004213fc56d5cc380fff3da9b01
kurionigiri <tsu181030@icloud.com>
2021-06-24 21:10:49 +0900

    ログイン機能実装

--- Views/home.php
+++ Views/home.php
@@ -1,76 +1,52 @@
-<?php
-// 設定関連を読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null ,
-        'tweet_created_at' => '2021-05-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました！',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-05-14 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ]
-];
-?>
 <!DOCTYPE html>
 <html lang="ja">
+ 
 <head>
-<?php include_once('../Views/common/head.php');?>
-    <title>ホーム画面/Twitterクローン</title>
+    <?php include_once('../Views/common/head.php'); ?>
+    <title>ホーム画面 / Twitterクローン</title>
+    <meta name="description" content="ホーム画面です">
 </head>
+ 
 <body class="home">
     <div class="container">
-    <?php include_once('../Views/common/side.php');?>
+        <?php include_once('../Views/common/side.php'); ?>
+ 
         <div class="main">
             <div class="main-header">
                 <h1>ホーム</h1>
             </div>
             <div class="tweet-post">
                 <div class="my-icon">
-                    <img src="<?php echo HOME_URL;?>Views/img_uploaded/user/sample-person.jpg" alt="">
+                    <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="">
                 </div>
                 <div class="input-area">
                     <form action="post.php" method="post" enctype="multipart/form-data">
-                        <textarea name="body" placeholder="今どうしてる？" maxlenght="140"></textarea>
-                            <div class="buttom-area">
-                                <div class="mb-0">
-                                    <input type="file" name="image" class="form-control form-control-sm">
-                                </div>  
-                                <buttom class="btn" type="submit">つぶやく</buttom>
+                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
+                        <div class="bottom-area">
+                            <div class="mb-0">
+                                <input type="file" name="image" class="form-control form-control-sm">
                             </div>
+                            <button class="btn" type="submit">つぶやく</button>
+                        </div>
                     </form>
                 </div>
             </div>
-
+ 
             <div class="ditch"></div>
-
+ 
             <?php if (empty($view_tweets)) : ?>
                 <p class="p-3">ツイートがまだありません</p>
             <?php else : ?>
                 <div class="tweet-list">
                     <?php foreach ($view_tweets as $view_tweet) : ?>
-                        <?php include('../Views/common/tweet.php');?>
+                        <?php include('../Views/common/tweet.php'); ?>
                     <?php endforeach; ?>
-                </div>      
+                </div>
             <?php endif; ?>
         </div>
     </div>
-    <?php include_once('../Views/common/foot.php');?>
+ 
+    <?php include_once('../Views/common/foot.php'); ?>
 </body>
+ 
 </html>
