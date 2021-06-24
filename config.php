b7ecc2249484f01b9a34d53b9b644a76b15ef7e3
kurionigiri <tsu181030@icloud.com>
2021-06-24 19:49:36 +0900

    MVCモデル実装

--- config.php
+++ config.php
@@ -3,3 +3,10 @@ ini_set('display_errors',1);
 
 date_default_timezone_set('Asia/Tokyo');
 define('HOME_URL','/twitterClone/');
+
+// データベースの接続情報
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'twitter_clone');
+ 
